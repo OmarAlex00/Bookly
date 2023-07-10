e7a2cc4ca9961a54a7a05adcf2c65a2c8251872b
Omar Ashraf <omarashraffci@gmail.com>
2023-07-10 16:44:16 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'features/splash/presentation/views/screens/splash_view.dart';
-
 void main() {
   runApp(const Bookly());
 }
