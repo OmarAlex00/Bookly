03be581e8315c1c20a3719127e875f1d7f032432
Omar Ashraf <omarashraffci@gmail.com>
2023-07-11 10:33:58 +0300

    using the XD icon

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class Asset {
   static const String kLogo = 'assets/images/Logo.png';
   static const String kPlaceHolderImg = 'assets/images/test_image.png';
+  static const String kSearchIcon = 'assets/images/zoom-out.png';
 }
--- lib/features/home/presentation/views/widgets/app_bar_search.dart
+++ lib/features/home/presentation/views/widgets/app_bar_search.dart
@@ -1,5 +1,5 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AppBarSearch extends StatelessWidget {
   const AppBarSearch({
@@ -8,12 +8,34 @@ class AppBarSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        splashRadius: 24,
-        onPressed: () {},
-        icon: const Icon(
-          FontAwesomeIcons.magnifyingGlass,
-          size: 25,
-        ));
+    return GestureDetector(
+      onTap: () {},
+      child: Image.asset(
+        Asset.kSearchIcon,
+        height: 20,
+        width: 20,
+      ),
+    );
   }
 }
+
+// -----------using font awesome icon----------------
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// class AppBarSearch extends StatelessWidget {
+//   const AppBarSearch({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return IconButton(
+//         splashRadius: 24,
+//         onPressed: () {},
+//         icon: const Icon(
+//           FontAwesomeIcons.magnifyingGlass,
+//           size: 25,
+//         ));
+//   }
+// }
