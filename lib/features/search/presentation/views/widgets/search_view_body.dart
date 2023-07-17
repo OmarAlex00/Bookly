196d9806e66c371dc45362004d4b68b9c9978c85
Omar Ashraf <omarashraffci@gmail.com>
2023-07-17 16:09:50 +0300

    ss

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/styles.dart';
+
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
@@ -9,6 +11,7 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextField(
             decoration: InputDecoration(
@@ -21,6 +24,7 @@ class SearchViewBody extends StatelessWidget {
                       opacity: 0.5, child: Image.asset(Asset.kSearchIcon)),
                 )),
           ),
+          const SearchResult()
         ],
       ),
     );
@@ -30,5 +34,22 @@ class SearchViewBody extends StatelessWidget {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(16),
         borderSide: const BorderSide(color: Colors.white));
+  }
+}
+
+class SearchResult extends StatelessWidget {
+  const SearchResult({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 30),
+      child: Text(
+        'Search Results',
+        style: Styles.youCanAlsoSee,
+      ),
+    );
   }
 }
