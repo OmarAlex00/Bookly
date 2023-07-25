e4e3568c46b64559c8460b0b6a18e9ac370369a7
Omar Ashraf <omarashraffci@gmail.com>
2023-07-25 16:06:07 +0300

    create error msg and loading indicator

---
+++ lib/core/widgets/error_text.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:flutter/widgets.dart';
+
+class ErrorText extends StatelessWidget {
+  const ErrorText({super.key, required this.errMsg});
+  final String errMsg;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        errMsg,
+        style: Styles.detailsBooksTitle,
+      ),
+    );
+  }
+}
