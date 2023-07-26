ce6af07211b87b4ca2bc37a159d77117a9e7dcf7
Omar Ashraf <omarashraffci@gmail.com>
2023-07-26 14:14:56 +0300

    books details done

--- lib/features/home/presentation/view_model/similar_books_cubit/similar_books_cubit.dart
+++ lib/features/home/presentation/view_model/similar_books_cubit/similar_books_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../data/models/book_model/book_model.dart';
 import '../../../data/repos/home_repo.dart';
@@ -9,7 +9,7 @@ part 'similar_books_state.dart';
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
   SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
   final HomeRepo homeRepo;
-  Future<void> fetchFeaturedBooks(String category) async {
+  Future<void> fetchSimilarBooks({required String category}) async {
     emit(SimilarBooksLoading());
     var results = await homeRepo.fetchSimilarBooks(categoryName: category);
     results.fold((failure) {
