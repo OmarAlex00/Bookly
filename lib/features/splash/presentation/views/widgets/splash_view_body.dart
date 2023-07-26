a2e8cc389a09ee05cae762fb7c825f567b9560ac
Omar Ashraf <omarashraffci@gmail.com>
2023-07-26 15:38:42 +0300

    solving back issue goes to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -57,7 +57,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHomeScreen() {
     Future.delayed(const Duration(seconds: 2), () {
-      GoRouter.of(context).push(AppRouter.kHomeScreenRouter);
+      GoRouter.of(context).pushReplacement(AppRouter.kHomeScreenRouter);
     });
   }
 }
