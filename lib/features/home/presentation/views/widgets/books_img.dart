4468cbf85dc82b35b1285b7e5b974c5f94cbe4ea
Omar Ashraf <omarashraffci@gmail.com>
2023-07-26 11:33:19 +0300

    setup cached network image

--- lib/features/home/presentation/views/widgets/books_img.dart
+++ lib/features/home/presentation/views/widgets/books_img.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/core/widgets/loading_indicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class BooksImg extends StatelessWidget {
@@ -8,15 +10,16 @@ class BooksImg extends StatelessWidget {
   final String url;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 0.67,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(url),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: AspectRatio(
+        aspectRatio: 0.67,
+        child: CachedNetworkImage(
+          imageUrl: url,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          progressIndicatorBuilder: (context, url, progress) =>
+              const LoadingIndicator(),
         ),
       ),
     );
